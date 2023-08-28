6e82f4c40ae2203619ca3ffa6b4ce5fe56dbf27e
Eric Bunton <eric.bunton@nutanix.com>
2023-08-28 11:42:08 -0400

    Typos in package script

--- bin/package.sh
+++ bin/package.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-set -x
+
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
-tar -C ${PROJECT_ROOT} -czf nutanix_foundation.tar.gz index.js package.json readme.md
+npm clean-install
+tar -C ${PROJECT_ROOT} -czf nutanix_foundation.tar.gz index.js package.json README.md
 tar -C ${PROJECT_ROOT} -czf dependencies.tar.gz node_modules
